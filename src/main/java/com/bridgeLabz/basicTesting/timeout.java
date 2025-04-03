10246421e3cfa98cc541bb0a1021c81501901f80
HarmanVeer24 <harmanveer0024@gmail.com>
2025-04-03 22:54:38 +0530

    [Harman] ADD:program to define the timeout class

---
+++ src/main/java/com/bridgeLabz/basicTesting/timeout.java
@@ -0,0 +1,11 @@
+package com.bridgeLabz.basicTesting;
+
+public class timeout {
+    public static void main(String[] args) {
+        System.out.println("Tesing in process....");
+    }
+
+    public static void longRunningTask(){
+        System.out.println("Process taking more than 2 sec")    ;
+    }
+}
