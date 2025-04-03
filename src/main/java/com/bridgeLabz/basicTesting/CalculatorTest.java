db852aa160fc9ba971096427759f39d15c2cff18
HarmanVeer24 <harmanveer0024@gmail.com>
2025-04-03 13:41:19 +0530

    [Harman] ADD:Calculator Class and methods

---
+++ src/main/java/com/bridgeLabz/basicTesting/CalculatorTest.java
@@ -0,0 +1,16 @@
+package com.bridgeLabz.basicTesting;
+
+public class CalculatorTest {
+    public static int sum(int data1,int data2){
+        return data1+data2;
+    }
+    public static int subtract(int data1,int data2){
+        return data1-data2;
+    }
+    public static int divison(int data1,int data2){
+        return data1/data2;
+    }
+    public static void main(String[] args) {
+       System.out.println("testing in progress");
+    }
+}
