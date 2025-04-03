e94b0d262738b56cea21e86954151351a78159b4
HarmanVeer24 <harmanveer0024@gmail.com>
2025-04-03 15:45:05 +0530

    [Harman] ADD:program to define the NumberCheck class and its methods

---
+++ src/main/java/com/bridgeLabz/basicTesting/NumberCheck.java
@@ -0,0 +1,10 @@
+package com.bridgeLabz.basicTesting;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        System.out.println("Testing in progress......");
+    }
+    public static boolean even(int data){
+        return data%2==0;
+    }
+}
